efe3f2dbd82bbe40b10aa9f6ec68ab4e4afd5c16
Hina Satarmaker <hina.satarmaker@cloudreach.com>
2022-08-03 23:16:14 +0530

    autoscaling group security group created

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "vpc_name" {
+    description = "This is the vpc name"
+}
+
+# variable "cidr_bastion_host" {
+#     description = "This is the CIDR range"
+# }
+
+variable "vpc_cidr" {
+    description = "This is the CIDR of VPC"
+}

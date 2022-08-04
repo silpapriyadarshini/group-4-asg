04f5e695fde24c6fc407a6595ec81f8d74361d96
shabana <shabana.bellary@cloudreach.com>
2022-08-04 12:55:14 +0530

    backend and provider files added

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "talent-academy-shoaib-lab-tfstates"
-        key = "talent-academy/sg_asg/terraform.tfstates"
-        region = "eu-west-1"
-        dynamodb_table = "terraform-lock"
-        
-    }
-}
